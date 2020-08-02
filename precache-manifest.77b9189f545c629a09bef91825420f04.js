edd32c8b0142fff38e22cb313351cf75507caddd
Prasham Ashesh <pashesh@allbound.com>
2020-08-02 16:19:13 +0530

    Updates

---
+++ precache-manifest.77b9189f545c629a09bef91825420f04.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892572e1c39ffa828ecde994c94cf408",
+    "url": "/edtech_editor/index.html"
+  },
+  {
+    "revision": "1370dfd183586474fb60",
+    "url": "/edtech_editor/static/css/2.472e899d.chunk.css"
+  },
+  {
+    "revision": "953340865bca551ce595",
+    "url": "/edtech_editor/static/css/main.302ba96f.chunk.css"
+  },
+  {
+    "revision": "1370dfd183586474fb60",
+    "url": "/edtech_editor/static/js/2.3c823efa.chunk.js"
+  },
+  {
+    "revision": "f8b0f4b14b85bbce75ab82edae26828b",
+    "url": "/edtech_editor/static/js/2.3c823efa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "953340865bca551ce595",
+    "url": "/edtech_editor/static/js/main.ca600305.chunk.js"
+  },
+  {
+    "revision": "7456ebc83d2f8288ea7e",
+    "url": "/edtech_editor/static/js/runtime-main.5fb42f06.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/edtech_editor/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/edtech_editor/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/edtech_editor/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/edtech_editor/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/edtech_editor/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/edtech_editor/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/edtech_editor/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/edtech_editor/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/edtech_editor/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/edtech_editor/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/edtech_editor/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/edtech_editor/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/edtech_editor/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/edtech_editor/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/edtech_editor/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/edtech_editor/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/edtech_editor/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
